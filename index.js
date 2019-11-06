7050b105acbb0be406ac38f757dc023118fae1ea
TTENDO <ttendobecky@gmail.com>
2019-11-06 09:51:59 +0300

    added the customised error page incase someone looks for a non existant page

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//creating the about us page
 app.get("/about", (req, res) => {
   res.send("This is the about us page");
+});
+
+//the post method
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+//the put method
+app.put("/", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+//the delete method
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
+//my error page
+app.get("*", (req, res) => {
+  res.send("Sorry non existent");
 });
