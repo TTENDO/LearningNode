13cdf5132bc41d0c45f799872530bd8d8a1ed81e
TTENDO <ttendobecky@gmail.com>
2019-11-06 09:19:01 +0300

    added the about us page

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // console.log("Hello World!!!")
 const express = require("express");
 const app = express(); //an app of type express
-app.listen(3000, function () { 
-    console.log('listening on 3000') 
-})
+app.listen(3000, function() {
+  console.log("listening on 3000");
+});
 
-app.get('/',  (req,  res) =>  {
-     res.send('Hello World') 
-    })
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the about us page");
+});
