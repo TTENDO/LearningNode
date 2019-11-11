e0dcbb4e5c0e71687d3738730a53812d91e768ba
TTENDO <ttendobecky@gmail.com>
2019-11-11 10:08:52 +0300

    layouts working on the idex.js file

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 // console.log("Hello World!!!")
 const express = require("express");
 const app = express(); //an app of type express
+
+//helps display posted data on the body
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const path = require("path"); //helps node understand my app directory
 app.set("view engine", "pug");
-
 app.set("views", path.join(__dirname, "views"));
+
+//getting the password and displaying it
+app.get("/first-template", (req, res) => {
+  // console.log("body", req.body);
+  // console.log("Query params", req.query);
+  res.render("regForm");
+});
+
+app.get("/first-template", (req, res) => {
+  res.send("regForm");
+});
+
+app.post("/first-template", (req, res) => {
+  console.log("this form has been posted");
+  console.log("body", req.body);
+  console.log("Query params", req.query);
+  res.send(req.body);
+  //res.render("regForm");
+});
+
+//adding route thanks where data is sent
+app.post("/thanks", (req, res) => {
+  /*console.log("this form has been posted");
+  console.log("body", req.body);
+  console.log("Query params", req.query);
+  //res.send(req.body);
+  res.send("Hello " + req.body.firstname); //this printds hello ree bcoz its what i input in the form*/
+
+  res.render("cohort",{
+    name: req.body.firstname + ' ' + req.body.lastname,
+    emailadd: req.body.email
+   
+  })
+
+  //res.render("regForm");
+});
 
 app.listen(3009, function() {
   console.log("listening on 3009");
@@ -22,12 +62,12 @@ server.on('listening', function () {
 
 server.listen(80); */
 
-
+/*
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/class", (req, res) => {
+app.get("/first-template", (req, res) => {
   res.render("regForm");
 });
 
@@ -52,7 +92,7 @@ app.put("/", (req, res) => {
 app.delete("/user", (req, res) => {
   res.send("Got a DELETE request at /user");
 });
-//path parameters
+//path parameters in the code and in the path
 app.get("/users/:name", (req, res) => {
   res.send("Hello " + req.params.name);
 });
@@ -60,7 +100,8 @@ app.get("/users/:name", (req, res) => {
 //query params -passed in the url
 app.get("/users", (req, res) => {
   res.send("This is class " + req.query.class + "cohort" + req.query.cohort);
-});
+});*/
+
 //my error page
 app.get("*", (req, res) => {
   res.send("Sorry non existent");
